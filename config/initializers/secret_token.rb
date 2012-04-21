c01e57317c122c053dbc1f71c99b22166769ef3f
Kevin Musiorski <musiorski@gmail.com>
2012-04-21 17:07:13 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Habitapp::Application.config.secret_token = 'd980ff884f1042d026d28d4a2cd4097daf5607b3b575813d9af668419ea57a2de472fa4b9a2cda746045b1ff530e87cb860f5a4cb737faad5fadb9b6542bfffa'
